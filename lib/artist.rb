6996fb066fe2edbe03321c74b5f917879e6a0609
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-09 15:59:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,14 @@ end
 
 def add_song(song)
   @songs << song
-  @@song_count += 1
+  # @@song_count += 1
   song.artist = self
 end
 
 def add_song_by_name(name)
   song = Song.new(name)
   @songs << song
+  @@song_count += 1
   song.artist = self
 end
 
